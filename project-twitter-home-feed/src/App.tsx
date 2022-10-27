632bffd925eb9d2709c641bb636483f0a0e8a27a
IasmimCristina <iaasmimcristinaa@gmail.com>
2022-10-27 17:57:14 -0300

    Adição da fonte do projeto.

--- project-twitter-home-feed/src/App.tsx
+++ project-twitter-home-feed/src/App.tsx
@@ -2,9 +2,15 @@ import { useState } from 'react'
 import './styles/global.css';
 
 export function App() {
-  
+
   return (
-    <h1> Hello, world!</h1>
+    <>
+      <h1 className='font-bold text-red-900'> Hello, world!</h1>
+
+      <button className='bg-red-400 hover:bg-white'>
+        enviar
+      </button>
+    </>
   )
 }
 
