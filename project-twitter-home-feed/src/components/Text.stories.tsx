5147f16fdc3aef572e231631af0b5af7140fe36d
IasmimCristina <iaasmimcristinaa@gmail.com>
2022-10-28 14:34:51 -0300

    Criação do componente de texto.

--- project-twitter-home-feed/src/components/Text.stories.tsx
+++ project-twitter-home-feed/src/components/Text.stories.tsx
@@ -1,8 +1,55 @@
-import { Text } from './Text'
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, Textprops } from './Text'
 
 export default {
   title: 'Components/Text',
   component: Text,
+  args: {
+    children: 'Example.'
+  },
+
+} as Meta<Textprops>
+
+
+export const Default: StoryObj<Textprops> = {}
+
+export const SuperExtraSmall: StoryObj<Textprops> = {
+  args: {
+    size: 'sxs',
+  }
+}
+export const ExtraSmall: StoryObj<Textprops> = {
+  args: {
+    size: 'xs',
+  }
+}
+export const Small: StoryObj<Textprops> = {
+  args: {
+    size: 'sm',
+  }
+}
+export const Medium: StoryObj<Textprops> = {
+  args: {
+    size: 'md',
+  }
+}
+export const Large: StoryObj<Textprops> = {
+  args: {
+    size: 'lg',
+  }
+}
+export const ExtraLarge: StoryObj<Textprops> = {
+  args: {
+    size: 'xlg',
+  }
 }
 
-export const Default = {}
+export const CustomComponent: StoryObj<Textprops> = {
+  args: {
+    asChild: true,
+    children: (
+      <p>Testing...</p>
+    )
+  }
+}
+
--- project-twitter-home-feed/src/components/Text.tsx
+++ project-twitter-home-feed/src/components/Text.tsx
@@ -1,5 +1,48 @@
-export function Text() {
+import { ReactNode } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+
+
+export interface Textprops {
+  size?: 'sxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xlg';
+  children: ReactNode;
+  isDark?: boolean;
+  isPrimary?: boolean;
+  asChild?: boolean;
+}
+export function Text({ size = 'md', isDark = false, isPrimary = true, children, asChild }: Textprops) {
+
+  const Comp = asChild ? Slot : 'span';
+
   return (
-    <h1>Text</h1>
+    <Comp className={
+      clsx(
+        'font-sans',
+        {
+          //Fazer a condição do texto nublado. 48min.
+
+          // Variações dark e light
+          'text-black': isDark === false && isPrimary,
+          'text-white': isDark === true && isPrimary,
+
+          'text-dark-5': isDark === false && isPrimary === false,
+          'text-dark-6': isDark === true && isPrimary === false,
+
+
+          //Variações do tamanho do texto
+          'text-sxs': size === 'sxs',
+          'text-xs': size === 'xs',
+          'text-sm': size === 'sm',
+          'text-md': size === 'md',
+          'text-lg': size === 'lg',
+          'text-xlg': size === 'xlg',
+
+
+
+        }
+
+      )}>
+      {children}
+    </Comp>
   )
 }
