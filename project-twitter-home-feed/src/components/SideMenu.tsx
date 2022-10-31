012a760dd35ace4b168415d0b4a477d2ef465458
IasmimCristina <iaasmimcristinaa@gmail.com>
2022-10-31 15:40:03 -0300

    Finalização do side menu e ínicio do componente News.

--- project-twitter-home-feed/src/components/SideMenu.tsx
+++ project-twitter-home-feed/src/components/SideMenu.tsx
@@ -19,94 +19,156 @@ export interface SideMenuProps {
 }
 export function SideMenu({ type = 'normal', }: SideMenuProps) {
 
-  // <Slot className={` w-7 h-6  ${props.style} `}>
-  //       {props.children}
-  //     </Slot>
 
-  {/* <a className={`font-sans font-bold text-lg ${props.style}`} href="#">{props.children}</a> */ }
-
-  {/* <div className={
-  `flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2  ${props.style}`
-
-}>
-  {props.children}
-
-</div> */}
 
   return (
-    <div className='h-screen bg-red  w-1/3 flex flex-col  justify-between'>
+    <div className='p-4 h-screen  w-1/3 flex flex-col  justify-between'>
       <div className="ml-10">
-        <TwitterLogo weight="fill" className={clsx(' h-7 w-6', {
-          'text-primary-blue': type == 'normal',
-          'text-white': type == 'dark',
-        })} />
-        <div className={
-          `flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 `
-        }>
-          <House />
-          <a className={`font-sans font-bold text-lg `} href="#">
-            Home
-          </a>
-        </div>
-        <div className={
-          `flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 `
-        }>
-          <HashStraight />
-          <a className={`font-sans font-bold text-lg `} href="#">
-            Explore
-          </a>
-        </div>
-        <div className={
-          `flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 `
-        }>
-          <Bell />
-          <a className={`font-sans font-bold text-lg `} href="#">
-            Notifications
-          </a>
-        </div>
-        <div className={
-          `flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 `
-        }>
-          <EnvelopeSimple />
-          <a className={`font-sans font-bold text-lg `} href="#">
-            Messages
-          </a>
-        </div>
-        <div className={
-          `flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 `
-        }>
-          <BookmarkSimple />
-          <a className={`font-sans font-bold text-lg `} href="#">
-            Bookmarks
-          </a>
-        </div>
-        <div className={
-          `flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 `
-        }>
-          <ListChecks />
-          <a className={`font-sans font-bold text-lg `} href="#">
-            Lists
-          </a>
-        </div>
-        <div className={
-          `flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 `
-        }>
-          <UserCircle />
-          <a className={`font-sans font-bold text-lg `} href="#">
-            Profile
-          </a>
-        </div>
-        <div className={
-          `flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 `
-        }>
-          <DotsThreeCircle />
-          <a className={`font-sans font-bold text-lg `} href="#">
-            More
-          </a>
+        <div className="ml-2">
+          <TwitterLogo weight="fill" className={clsx(' h-7 w-6', {
+            'text-primary-blue': type == 'normal',
+            'text-white': type == 'dark',
+          })} />
+          <div className={clsx(`flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 border-primary-blue`, {
+
+          })
+
+          }>
+            <House className='text-primary-blue text-lg ml-2 ' />
+            <a className={`font-sans font-bold text-lg text-primary-blue `} href="#">
+              Home
+            </a>
+          </div>
+          <div className={clsx(`flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 `, {
+            'border-primary-blue': type == 'normal',
+            'border-white': type == 'dark',
+
+          })
+
+          }>
+            <HashStraight className={clsx(`text-lg   ml-2 `, {
+              'text-black': type == 'normal',
+              'text-white': type == 'dark',
+            })} />
+            <a className={clsx(`font-sans font-bold text-lg  `, {
+              'text-black': type == 'normal',
+              'text-white': type == 'dark',
+            })} href="#">
+              Explore
+            </a>
+          </div>
+          <div className={clsx(`flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 `, {
+            'border-primary-blue': type == 'normal',
+            'border-white': type == 'dark',
+
+          })
+
+          }>
+            <Bell className={clsx(`text-lg  ml-2 `, {
+              'text-black': type == 'normal',
+              'text-white': type == 'dark',
+            })} />
+            <a className={clsx(`font-sans font-bold text-lg  `, {
+              'text-black': type == 'normal',
+              'text-white': type == 'dark',
+            })} href="#">
+              Notifications
+            </a>
+          </div>
+          <div className={clsx(`flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 `, {
+            'border-primary-blue': type == 'normal',
+            'border-white': type == 'dark',
+
+          })
+
+          }>
+            <EnvelopeSimple className={clsx(`text-lg   ml-2`, {
+              'text-black': type == 'normal',
+              'text-white': type == 'dark',
+            })} />
+            <a className={clsx(`font-sans font-bold text-lg  `, {
+              'text-black': type == 'normal',
+              'text-white': type == 'dark',
+            })} href="#">
+              Messages
+            </a>
+          </div>
+          <div className={clsx(`flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 `, {
+            'border-primary-blue': type == 'normal',
+            'border-white': type == 'dark',
+
+          })
+
+          }>
+            <BookmarkSimple className={clsx(`text-lg   ml-2`, {
+              'text-black': type == 'normal',
+              'text-white': type == 'dark',
+            })} />
+            <a className={clsx(`font-sans font-bold text-lg  `, {
+              'text-black': type == 'normal',
+              'text-white': type == 'dark',
+            })} href="#">
+              Bookmarks
+            </a>
+          </div>
+          <div className={clsx(`flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 `, {
+            'border-primary-blue': type == 'normal',
+            'border-white': type == 'dark',
+
+          })
+
+          }>
+            <ListChecks className={clsx(`text-lg   ml-2`, {
+              'text-black': type == 'normal',
+              'text-white': type == 'dark',
+            })} />
+            <a className={clsx(`font-sans font-bold text-lg  `, {
+              'text-black': type == 'normal',
+              'text-white': type == 'dark',
+            })} href="#">
+              Lists
+            </a>
+          </div>
+          <div className={clsx(`flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 `, {
+            'border-primary-blue': type == 'normal',
+            'border-white': type == 'dark',
+
+          })
+
+          }>
+            <UserCircle className={clsx(`text-lg   ml-2`, {
+              'text-black': type == 'normal',
+              'text-white': type == 'dark',
+            })} />
+            <a className={clsx(`font-sans font-bold text-lg  `, {
+              'text-black': type == 'normal',
+              'text-white': type == 'dark',
+            })} href="#">
+              Profile
+            </a>
+          </div>
+          <div className={clsx(`flex justify-start  gap-4 h-16 w-64  items-center cursor-pointer transition-all hover:border-box hover:border-2 `, {
+            'border-primary-blue': type == 'normal',
+            'border-white': type == 'dark',
+
+          })
+
+          }>
+            <DotsThreeCircle className={clsx(`text-lg   ml-2`, {
+              'text-black': type == 'normal',
+              'text-white': type == 'dark',
+            })} />
+            <a className={clsx(`font-sans font-bold text-lg  `, {
+              'text-black': type == 'normal',
+              'text-white': type == 'dark',
+            })} href="#">
+              More
+            </a>
+          </div>
         </div>
         <button className={
           clsx(
-            'font-sans font-bold rounded-full transition-all border-2 focus:ring-4 ring-dark-5 py-3 px-24 text-white   bg-primary-blue hover:text-primary-blue  hover:bg-white  border-primary-blue',
+            'mt-2 font-sans font-bold rounded-full transition-all border-2 focus:ring-4 ring-dark-5 py-3 px-24 text-white   bg-primary-blue hover:text-primary-blue  hover:bg-white  border-primary-blue',
             {
             }
           )}>
@@ -119,16 +181,25 @@ export function SideMenu({ type = 'normal', }: SideMenuProps) {
           <div className="flex flex-row gap-2">
             <img className='h-10 w-10 rounded-full ml-2 bg-white text-sxs text-primary-blue border-0' src={image.src} alt={image.alt} />
             <div className="flex flex-col ">
-              <span className={` text-md  `}>Davide Biscuso</span>
               <span className={clsx(` text-md  `, {
+                'text-black': type == 'normal',
+                'text-white': type == 'dark',
+
+              })}>Davide Biscuso</span>
+              <span className={clsx(` text-md  `, {
+                'text-dark-5': type == 'normal',
+                'text-dark-6': type == 'dark',
+
               })}>@biscuttu</span>
             </div>
           </div>
-          <DotsThreeOutline className={clsx('', {
+          <DotsThreeOutline className={clsx(' transition-all w-12 h-4 hover:w-14 hover:h-6 cursor-pointer hover:text-primary-blue', {
 
+            'text-black': type == 'normal',
+            'text-white': type == 'dark',
           })} />
         </div>
-        
+
       </div>
 
 
