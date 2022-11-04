d65e841ecec10e3d5f7845215223991355f94d7e
IasmimCristina <iaasmimcristinaa@gmail.com>
2022-11-04 12:24:52 -0300

    Refatoração - Header e Menuitem

--- project-twitter-home-feed/src/components/Header.tsx
+++ project-twitter-home-feed/src/components/Header.tsx
@@ -1,23 +1,58 @@
 import { ReactNode } from 'react';
 import { clsx } from 'clsx';
-import { Sparkle } from "phosphor-react";
+import { Sparkle, ArrowLeft } from "phosphor-react";
 
 
 export interface HeaderProps {
 
   type?: 'dark' | 'normal';
+  page?: 'Home' | 'Profile';
 
 }
-export function Header({ type = 'normal' }: HeaderProps) {
+export function Header({ type = 'normal', page = 'Home' }: HeaderProps) {
+  let profileContent;
+  if (page === 'Profile') {
+    profileContent = {
+      content: <span className={clsx('text-sxs inline-block leading-3', {
+        'text-dark-5': type === 'normal',
+        'text-dark-6': type === 'dark',
+
+      })} > 9 tweets </span>,
+      name: 'Name',
+      icon: <div className=" w-9 h-8">
+        <ArrowLeft weight="bold" className={clsx('text-primary-blue w-8 h-7 cursor-pointer transition-all hover:w-9 hover:h-8  ', {
+          'hover:text-black ': type === 'normal',
+          'hover:text-white ': type === 'dark',
+        })} />
+      </div>,
+    };
+  } else {
+    profileContent = {
+      content: "",
+      name: 'Home',
+      icon: '',
+    };
+  }
+
+
 
   return (
-    <div className='h-4 w-1/3 p-2' >
-      <div className="flex justify-between">
-        <h1 className={clsx('font-bold text-lg', {
-            'text-white': type === 'dark',
+    <div className='h-4  p-2' >
+      <div className="flex justify-between items-center">
 
-            'text-black': type === 'normal',
-        })}>Home</h1>
+        <div className=" flex  items-center ">
+          {profileContent.icon}
+          <div className={clsx('', {
+            'ml-10': page === 'Profile'
+          })}>
+            <h1 className={clsx('font-bold text-lg leading-5 ', {
+              'text-black': type === 'normal',
+              'text-white': type === 'dark',
+
+            })}>{profileContent.name}</h1>
+            {profileContent.content}
+          </div>
+        </div>
 
         <div className={clsx(' cursor-pointer h-8 text-primary-blue  flex  content-center', {
 
